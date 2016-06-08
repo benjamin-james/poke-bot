cc669382d7d710deb0daff818a7b436f0c06376a
Benjamin James <bjames@openmailbox.org>
2016-06-08 16:05:10 -0500

    uses env or cmdline email & asks for password

--- configure.py
+++ configure.py
@@ -1,3 +1,9 @@
-username = 'bob@example.com'
-passwd = 'password'  # could use getpass.getpass('Enter password:')
+import getpass
+import sys
+import os
+
+username = os.getenv('EMAIL', 'you@example.com')
+if len(sys.argv) > 1:
+        username = sys.argv[1]
+passwd = getpass.getpass('Enter password for %s: ' % username)
 delay = 5.0  # in seconds
